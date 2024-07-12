7f246882926a66c963c5b8da19b851035a14b997
mapachurro <oliver.renwick@consensys.net>
2024-07-12 11:51:29 -0400

    adjust baseurl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,15 +8,15 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Education DAO',
+  title: 'Education DAO documentation',
   tagline: 'Preserve and maintain the collective knowledge of web3, sustainably',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://educationdao.xyz',
+  url: 'https://education-dao-organization.github.io/educationdao.xyz/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/educationdao.xyz/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
